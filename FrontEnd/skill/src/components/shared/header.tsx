d3e00e18d22c2be5b103d740f2e2c8aea960cb19
ndkazu <ndongmefane@gmail.com>
2024-05-06 14:07:49 +0900

    More work on Task Market Page 1

--- FrontEnd/skill/src/components/shared/header.tsx
+++ FrontEnd/skill/src/components/shared/header.tsx
@@ -4,7 +4,7 @@ import AccountModal from './AccountModal';
 export default function Header() {
   return (
     <div className="h-20 bg-gradient-to-r from-pink-700 to-blue-500 px-4 flex justify-between items-center">
-      <div className=" font-bold text-4xl antialiased text-white">Skill Master</div>
+      <div className=" font-bold text-4xl antialiased text-white">Skill Tracker</div>
       <AccountModal />
     </div>
   );
